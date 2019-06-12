aae0a84861bb197bb2900448522894c775a7e783
Artem <syanches@gmail.com>
2019-06-12 18:11:03 +0300

    Initial Commit

---
+++ BookShelf/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BookShelf
+//
+//  Created by Артем Мартиросян on 12/06/2019.
+//  Copyright © 2019 Артем Мартиросян. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
